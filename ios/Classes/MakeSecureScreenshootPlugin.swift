00d6218514df58de8a8449d1f845f013b2e6e9db
Mikl.Night <miklce@mail.ru>
2024-03-02 02:39:01 +0300

    add swift module

--- ios/Classes/MakeSecureScreenshootPlugin.swift
+++ ios/Classes/MakeSecureScreenshootPlugin.swift
@@ -16,4 +16,41 @@ public class MakeSecureScreenshootPlugin: NSObject, FlutterPlugin {
       result(FlutterMethodNotImplemented)
     }
   }
+
+  extension UIWindow {
+    func makeSecure() -> UITextField? {
+      let field = UITextField()
+      field.isSecureTextEntry = true
+      field.tag = 100
+      self.addSubview(field)
+      field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+      field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+
+      self.layer.superlayer?.addSublayer(field.layer)
+      field.layer.sublayers?.first?.addSublayer(self.layer)
+
+
+      let label = UILabel()
+          label.text = ""
+          label.tag = 100
+          label.textColor = .white
+          label.font = UIFont.boldSystemFont(ofSize: 8)
+          label.sizeToFit()
+          label.center = self.center
+
+      self.addSubview(label)
+      return field;
+    }
+
+    func removeSecure() {
+      for subview in self.subviews {
+       if let textField = subview as? UITextField, textField.tag == 100 {
+           textField.removeFromSuperview()
+       }
+       if let label = subview as? UILabel, label.tag == 100 {
+          label.removeFromSuperview()
+       }
+      }
+    }
+  }
 }
