950941c64c11edbd1a621f41f4affe89951ff479
Mikl.Night <miklce@mail.ru>
2024-03-02 04:27:41 +0300

    build example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  bool? _secureStatus;
   final _makeSecureScreenshootPlugin = MakeSecureScreenshoot();
 
   @override
@@ -25,21 +26,14 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _makeSecureScreenshootPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _makeSecureScreenshootPlugin.getPlatformVersion() ?? 'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
@@ -47,15 +41,73 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Future<void> getSecureStatus() async {
+    bool? status;
+    try {
+      status = await _makeSecureScreenshootPlugin.getSecureStatus();
+      setState(() => _secureStatus = status);
+    } on PlatformException {
+      status = null;
+    }
+  }
+
+  Future<void> makeSecure() async {
+    var result = await _makeSecureScreenshootPlugin.makeSecure();
+    setState(() => _secureStatus = result);
+    //getSecureStatus();
+  }
+
+  Future<void> removeSecure() async {
+    await _makeSecureScreenshootPlugin.removeSecure();
+    getSecureStatus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        //backgroundColor: Colors.grey,
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Make Secure Screenshoot'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Current status: $_secureStatus\n', style: const TextStyle(fontSize: 16)),
+                  ElevatedButton(onPressed: () => getSecureStatus(), child: const Text("Refresh")),
+                  const SizedBox(height: 16.0),
+                  Builder(builder: (context) {
+                    if (_secureStatus ?? false) {
+                      return const Text(
+                        "If you take a screenshot or start recording the screen, you will get a black screen",
+                        textAlign: TextAlign.center,
+                      );
+                    } else {
+                      return const Text(
+                        "Secure screen is not active, you can take screenshots",
+                        textAlign: TextAlign.center,
+                      );
+                    }
+                  })
+                ],
+              ),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: () => makeSecure(), child: const Text("Make Secure")),
+                  ElevatedButton(onPressed: () => removeSecure(), child: const Text("Remove Secure")),
+                ],
+              ),
+              Text('Running on: $_platformVersion\n'),
+            ],
+          ),
         ),
       ),
     );
